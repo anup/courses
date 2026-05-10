c7420331eb29351b914e55b33f12561c8184a0be
Anup Kalbalia <anup.kalbalia@gmail.com>
2026-05-10 23:17:50 +0530

    Update ES248 API endpoint

--- es248/config.js
+++ es248/config.js
@@ -1,5 +1,5 @@
 // Set this once after deploying the Apps Script web app.
 // This file is separate so leaderboard.html changes don't overwrite it.
 var ES248_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbzxzochuqX37z853ES0iboKQamwwpzFSaxobbuFv4cW9SlYZJ82h6eKkxvqPw_ZVY7g/exec"
+  API_BASE: "https://script.google.com/macros/s/AKfycbyoiHVG5I7a9Wj5XvaNjl_acP5jnm69YNtTZWpfCopOex7ue5OiVeb_EN_79fSdZI93/exec"
 };
