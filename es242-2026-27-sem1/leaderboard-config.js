2775519ce809177153565276d5ce663024134bd8
Anup Kalbalia <anup.kalbalia@gmail.com>
2026-08-04 04:04:46 +0530

    Publish Semester I 2026-27 automation links

---
+++ es242-2026-27-sem1/leaderboard-config.js
@@ -0,0 +1,3 @@
+var ES242_LEADERBOARD_CONFIG = {
+  API_BASE: "https://script.google.com/macros/s/AKfycbyqbW9Bk49oDAj7MPyk4jLzKn0al02nA-1vns-0dDIR4ew6YyvA8NkvFDzw1KGueEMw/exec"
+};
---
+++ es248-2026-27-sem1/leaderboard-config.js
@@ -0,0 +1,3 @@
+var ES248_LEADERBOARD_CONFIG = {
+  API_BASE: "https://script.google.com/macros/s/AKfycbzOvyGVAWEBb8b4uSUX8xIwcm8TRoZGrZ8dbpX3gx5JoB9arBysr6nJfppArNNCiISjZg/exec"
+};
