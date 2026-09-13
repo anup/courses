57a21b3bfe96a5089694e8f463d0fab3b0c88767
Anup Kalbalia <anup.kalbalia@gmail.com>
2026-09-13 12:19:53 +0530

    ES242: explain current graded score ceilings

--- es242-2026-27-sem1/leaderboard-config.js
+++ es242-2026-27-sem1/leaderboard-config.js
@@ -1,3 +1,3 @@
 var ES242_LEADERBOARD_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbyqbW9Bk49oDAj7MPyk4jLzKn0al02nA-1vns-0dDIR4ew6YyvA8NkvFDzw1KGueEMw/exec"
+  API_BASE: "https://script.google.com/macros/s/AKfycbyfvHcwh11GTV8ixkziub9oYjyMQlrjkQLXokL8DU34YUq8_NkohhF7E0DHO9vuwE8K/exec"
 };
